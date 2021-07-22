97caf26c8e79fc26b1e2b7cd6caa24a4abb861fa
CristianMartinez1997 <87827956+CristianMartinez1997@users.noreply.github.com>
2021-07-22 13:22:43 -0500

    Add files via upload

---
+++ Informacion.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Informacion CCP</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div class="contenido2">
+		<a href="index.php">Volver</a>
+		<h2>Nuestra historia...</h2>
+		<p>Fundado el 7 de Junio de 1995 por iniciativa de seis docentes, en cabeza de Gladis María Núñez Camacho, en compañía de María Stella Mosquera Monteros, Ruby Constanza Tumiñá Bolívar, Diego López Daza,  Luis Ángel Meneses Papamija (q.e.p.d.), Carlos Augusto Ramirez Paz (q.e.p.d.);  interesados en la búsqueda de nuevas alternativa para el municipio,  iniciaron con el nivel de preescolar desde pre jardín niños o niñas de 3 años, jardín niños o niñas de cuatro años y transición de cinco años  y el grado primero, luego se fueron incrementando los grados de la básica primaria hasta la básica secundaria y  media vocacional teniendo la primera promoción de preescolar realizando ceremonia de grados en el año de 1996 y de la media vocacional en el año 2000.    Se dieron a la tarea de diseñar los símbolos institucionales como son: el escudo del colegio, la bandera y  su nombre, aduciendo que en el futuro Piendamó sería una ciudad, se definieron los colores Rojo, gris y blanco como los colores institucionales; desde entonces la institución se viene destacando por el fomento del folclor y la identidad cultural teniendo como áreas optativas el área de danzas, música y artes las cuales permiten el desarrollo de actividades como peñas artísticas, encuentros culturales y comunitarios donde participan estudiantes, padres de familia y docentes.</p>
+
+		<p>Los primeros 12 años prestó los servicios educativos en la infraestructura de la Institución Educativa Simón Bolívar, de los cuales  9 años fue dirigida por la esp. María Stella Mosquera, quien cedió su cargo en el año  2004 a la Esp. María Isabel Muñoz García quien desde su gestión lideró procesos de desarrollo organizacional y participativo, obteniendo como resultado el fortalecimiento de la imagen institucional a nivel local, regional y nacional. Con la categorización de la institución en A+ desde 2008 hasta la actualidad.   Desde éste año se fortaleció la Fundación Colegio Ciudad de Piendamó con el ingreso de nuevos integrantes, a fin de contribuir a los procesos educativos y de gestión desde la organización, entre ellos: César Eduardo Canencio Rivera, Diana Londoño Buitrago, Willian Efraín Timana Gutiérrez, Stella Corina Hurtado Maturana, Kelly Johana Concha Astudillo,  Meyer Felina Daza Paz (q.e.p.d.) Gloria Cecilia Paz López.</p>
+
+		<p>Instalados desde 2008 a 2010  en la casa de propiedad de la Parroquia en el Centro de Piendamó cuyo nombre es Nuestra Señora del Camino, periodo transitorio mientras se iniciaban obras de la primera etapa de construcción en la sede propia ubicada en la calle 2 No. 4 – 80 Barrio San Cayetano, lugar estratégicamente ubicado con amplias zonas verdes, fácil acceso vehicular y peatonal, excelentes escenarios deportivos de carácter público.   Por la disposición de número de aulas se funcionó en dos jornadas, mañana y tarde;   tres años después se logró la construcción de la segunda etapa del proyecto de infraestructura que consistió en la cancha múltiple, 5 aulas y batería sanitaria, que permitió el funcionamiento de la institución en una sola jornada, facilitando la operatividad de la labor docente y administrativa; así mismo se inicia el proceso de mejoramiento en la contratación del personal docente, del cual es parte fundamental la formalización y conformación de la asociación de padres de familia “Asopadres CCP” 13 febrero de 2017.</p>
+
+		<p>En el 2015, etapa durante la cual se vinculan los miembros a la Fundación Edgar Alfonso Paja Flor y Astrid Milena Meneses Franco; y se emprende la construcción del área administrativa y cafetería como parte del plan de mejoramiento institucional y se garantiza la contratación del personal docente en el 98% de tiempo completo.</p>
+
+		<p>En el año lectivo 2019, se presenta la oportunidad de fortalecer los procesos administrativos, pedagógicos y de gestión desde la representación legal a cargo de María Isabel Muñoz García, y por ello asume la rectoría la profesional Maribel Montealegre Pérez, quien ejercía el cargo de Revisora Fiscal desde del año 2005 y actualmente contribuye a la dinámica académica en la prestación del servicio educativo de la institución.</p>
+
+		<p>Finalizando 2019 e inicios 2020 Se da inicio a la cuarta etapa de construcción a fin de garantizar la atención a la proyección matricula estudiantil y los nuevos proyectos de la Fundación Colegio Ciudad de Piendamó como es la modernización institucional a través de procesos de calidad.
+
+		Los primeros 25 años de una institución educativa vienen construyendo huella en más de 250 egresados y sus familias que hacen gozar de una imagen de responsabilidad y compromiso con la región.</p>
+
+
+		<p>El año 2020 ha marcado la historia del mundo y el Colegio Ciudad de Piendamó no ha sido ajeno a la situación,  sin tener la posibilidad de tener ningún acercamiento social la institución se enfrentó a rediseñar sus estrategias de aprendizaje a través de los medios digitales, a fin de velar por el cuidado y salud de todas las familias;   situación que nos ha permitido enriquecer los procesos enseñanza – aprendizaje con herramientas didácticas que hoy por hoy se encuentran disponibles y facilitan el trabajo remoto de las actividades académicas.  finalmente el 2020 culminó con la satisfacción de alcanzar las metas institucionales propuestas  y la promoción de más del  97% de la población estudiantil.    Así mismo paralelo al desarrollo de los planes académicos y pedagógicos, se logró el proceso de resignificación del PEI  «Impulsando sueños, transformando realidades»,  como nuestro nuevo reto institucional 2021 – 2030.</p>
+
+	</div>
+
+</body>
+</html>
